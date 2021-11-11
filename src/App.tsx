2914f767c8f3aef0d3d73ebae9ff7b6ac1c72783
Jesper Kihlberg <jesper.kihlberg@gmail.com>
2021-11-11 09:04:19 +0100

    Cleanup and package update

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,5 @@
 import { AppBar, Avatar, Button, createStyles, Icon, IconButton, makeStyles, SvgIcon, Toolbar, Typography } from "@material-ui/core";
 import React from "react";
-import Questions from "./components/quiz-helpers/Questions";
-import SimpleFunctionalComponent from "./components/samples/SimpleFunctionalComponent";
-import UseStateComponent1 from "./components/samples/UseStateComponent1";
-import UseStateComponent2 from "./components/samples/UseStateComponent2";
-import UseStateComponent3 from "./components/samples/UseStateComponent3";
-import UseStateComponent4 from "./components/samples/UseStateComponent4";
-import UseStateComponent5 from "./components/samples/UseStateComponent5";
-import UseStateComponent6 from "./components/samples/UseStateComponent6";
-import UseStateComponent7 from "./components/samples/UseStateComponent7";
-
-var SimpleFunctionalComponentText = require("../public/samples/SimpleFunctionalComponent.txt");
-var UseStateComponentComponentText1 = require("../public/samples/UseStateComponent1.txt");
-var UseStateComponentComponentText2 = require("../public/samples/UseStateComponent2.txt");
-var UseStateComponentComponentText3 = require("../public/samples/UseStateComponent3.txt");
-var UseStateComponentComponentText4 = require("../public/samples/UseStateComponent4.txt");
-var UseStateComponentComponentText5 = require("../public/samples/UseStateComponent5.txt");
-var UseStateComponentComponentText6 = require("../public/samples/UseStateComponent6.txt");
-var UseStateComponentComponentText7 = require("../public/samples/UseStateComponent7.txt");
 
 const useStyles = makeStyles(() =>
   createStyles({
@@ -47,7 +29,7 @@ const App: React.FC = () => {
             <Avatar aria-label="recipe" style={{ backgroundColor: "transparent" }}>
               <img className={classes.imageIcon} src="./favicon.svg" />
             </Avatar>
-            <Button color="inherit">React component lifecycle quiz</Button>
+            <Button color="inherit">React TypeScript Starter</Button>
           </Toolbar>
         </AppBar>
       </div>
